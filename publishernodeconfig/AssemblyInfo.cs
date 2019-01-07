1d541aa4a5042db08920a58532a0627ef524800e
Hans Gschossmann <johanng@microsoft.com>
2019-01-07 11:48:54 +0100

    add gitversion

---
+++ publishernodeconfig/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿
+[assembly: System.Reflection.AssemblyCompany("Microsoft")]
+[assembly: System.Reflection.AssemblyConfiguration("Release")]
+[assembly: System.Reflection.AssemblyDescription("An application to configure OPC Publisher nodes remotely.")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.1+Branch.gitversion.Sha.3187260f464e97769278e7da77a24a93879a0bb7")]
+[assembly: System.Reflection.AssemblyProduct("publishernodeconfig")]
+[assembly: System.Reflection.AssemblyTitle("publishernodeconfig")]
+[assembly: System.Reflection.AssemblyVersion("1.0.1.0")]
