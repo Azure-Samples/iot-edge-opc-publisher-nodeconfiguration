15534865e094ec9b1cdf9e204869333a554817e9
Hans Gschossmann <johanng@microsoft.com>
2019-02-13 21:58:18 -0800

    update models

--- publishernodeconfig/AssemblyInfo.cs
+++ publishernodeconfig/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: System.Reflection.AssemblyConfiguration("Release")]
 [assembly: System.Reflection.AssemblyDescription("An application to configure OPC Publisher nodes remotely.")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0+Branch.master.Sha.b88d1fc6e9b2cb1642f1b8a97d84dab4d564a9eb")]
 [assembly: System.Reflection.AssemblyProduct("publishernodeconfig")]
 [assembly: System.Reflection.AssemblyTitle("publishernodeconfig")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
